e65609a5f6e67db0f7138c142b5d5b5e34de81f1
Ohno <51406176+Ohno-Tk@users.noreply.github.com>
2022-10-12 23:25:00 +0900

    [change]画面のちらつき防止

--- js/GetImageFile.js
+++ js/GetImageFile.js
@@ -1,29 +1,27 @@
 $(window).on('load', function()
 {
-  sliderInit();
-
   $.ajax(
-  {
-    url:'imagelist.txt',
-
-    // 成功時
-    success: function(data)
     {
-      // データ区切り(LF、CRLF、CR)
-      var data_array = data.split(/\r\n|\r|\n/);
-      var img;
-      var a = document.getElementById('qqq')
+      url:'imagelist.txt',
 
-      // 先頭が空白以外の場合
-      if (data_array[0] != '')
+      // 成功時
+      success: function(data)
       {
-        for (var i = 0; i < data_array.length; i++)
+        // データ区切り(LF、CRLF、CR)
+        var data_array = data.split(/\r\n|\r|\n/);
+        var img;
+        var divElement = document.getElementById('qqq');
+
+        // 先頭が空白以外の場合
+        if (data_array[0] != '')
         {
-          console.log(data_array[i]);
-          // 子要素に追加
-          img = document.createElement('img');
-          img.src = "image/" + data_array[i];
-          a.appendChild(img);
+          for (var i = 0; i < data_array.length; i++)
+          {
+            // 子要素に追加
+            img = document.createElement('img');
+            img.src = "image/" + data_array[i];
+            divElement.appendChild(img);
+          }
         }
 
         // slickの破棄
@@ -32,22 +30,20 @@ $(window).on('load', function()
         // スライダー初期化
         sliderInit();
       }
-    }
-  });
-
-  // スライダー初期化
-  function sliderInit(){
-    $('.slider').slick({
-      autoplay: true,//自動的に動き出すか。
-      speed:1000,//スライドの動きのスピード。
-      slidesToShow: 1,//スライドを画面に3枚見せる
-      slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
-      arrows: false,//左右の矢印あり
-      dots: true, // ドットナビゲーションを表示する
-      pauseOnFocus: false,//フォーカスで一時停止を無効
-      pauseOnHover: false,//マウスホバーで一時停止を無効
-      pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
     });
-  }
-
 });
+
+// スライダー初期化
+function sliderInit(){
+  $('.slider').slick({
+    autoplay: true,//自動的に動き出すか。
+    speed:1000,//スライドの動きのスピード。
+    slidesToShow: 1,//スライドを画面に3枚見せる
+    slidesToScroll: 1,//1回のスクロールで3枚の写真を移動して見せる
+    arrows: false,//左右の矢印あり
+    dots: true, // ドットナビゲーションを表示する
+    pauseOnFocus: false,//フォーカスで一時停止を無効
+    pauseOnHover: false,//マウスホバーで一時停止を無効
+    pauseOnDotsHover: false,//ドットナビゲーションをマウスホバーで一時停止を無効
+  });
+}
